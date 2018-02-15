90030a5876f13ec536c45bfc9802afcf6c51a6c0
kireizi <falloutyou@ya.ru>
2018-02-15 22:05:58 +0600

    Сделал класс "Дисциплины", с DAO не получается разобраться.

---
+++ src/main/java/kz/keu/ipp/entity/Discipline.java
@@ -0,0 +1,40 @@
+package kz.keu.ipp.entity;
+
+public class Discipline {
+    private int id;
+    private String code;
+    private String name;
+
+    public Discipline(int id, String code, String name) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getcode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
